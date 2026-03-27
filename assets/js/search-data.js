0c526b1d1002756acb7f3bdcc86f0a0c0b1d486e
trjhuang <jenson.huang0724@gmail.com>
2026-03-27 07:17:46 +0000

    Deploying to gh-pages from @ trjhuang/trjhuang.github.io@8a77c76cc3a3fb3e845557d1f79332cec7870de7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-started-my-undergraduate-studies-at-georgia-tech",
+          title: 'I started my undergraduate studies at Georgia Tech.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
