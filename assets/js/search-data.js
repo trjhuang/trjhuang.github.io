6364c6f0bd0d6ece535c1f9ae61a28fab68cc101
trjhuang <jenson.huang0724@gmail.com>
2026-03-27 07:25:42 +0000

    Deploying to gh-pages from @ trjhuang/trjhuang.github.io@942dc0910d9f124dc8bb15278aa57c95d955d921 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,11 +28,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-i-started-my-undergraduate-studies-at-georgia-tech",
           title: 'I started my undergraduate studies at Georgia Tech.',
           description: "",
