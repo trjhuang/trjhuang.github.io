fc2434a1f3ff96e00aa2f16d660c8fd144a6140f
trjhuang <jenson.huang0724@gmail.com>
2026-03-31 09:37:46 +0000

    Deploying to gh-pages from @ trjhuang/trjhuang.github.io@481a0fdcdc1305f18575d9feb750fc82fb1628a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-resume",
           title: "resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Hey! Here&#39;s my resume. Feel free to view it below or download a copy.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
